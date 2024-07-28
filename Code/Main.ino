143bc33b73ca0adbf75037bc81e449c76582d7b6
KavEn06 <87994667+KavEn06@users.noreply.github.com>
2024-07-27 23:01:34 -0400

    Update Main.ino
    Descriptions

--- Code/Main.ino
+++ Code/Main.ino
@@ -1,3 +1,24 @@
+/*
+  Main Buzzer Code
+  ----------------
+  This code runs on the main ESP32 development board in a wireless buzzer system designed for quiz or game setups. 
+  It manages the central communication and control of the buzzer system, displaying the pressed buttons on an I2C 16x2 LCD display.
+  The system supports 4 teams with 4 players each and uses the ESPNOW protocol for communication, achieving a latency 
+  response time of approximately 2 milliseconds.
+
+  Features:
+  - Receives button press data from team buzzers
+  - Displays the pressed buttons on an I2C 16x2 LCD
+  - Manages the state of button presses to ensure correct game flow
+  - Resets the system when the reset button is pressed
+
+  Components:
+  - 1 x ESP32 Development Board (Main Buzzer)
+  - 1 x I2C 16x2 LCD Display
+  - 1 x Push Button
+  Author: Kavin Nimalarajan
+*/
+
 // include libraries
 #include <esp_now.h>
 #include <WiFi.h>
@@ -16,10 +37,10 @@ bool Cpress = false;
 bool Dpress = false; 
 
 // MAC address' of buzzers
-uint8_t teamA[] = { 0xA8, 0x42, 0xE3, 0xBC, 0x72, 0xF8 };
-uint8_t teamB[] = { 0xE4, 0x65, 0xB8, 0x79, 0x78, 0xB0 };
-uint8_t teamC[] = { 0xA0, 0xA3, 0xB3, 0xAB, 0x84, 0x24 };
-uint8_t teamD[] = { 0xE4, 0x65, 0xB8, 0x79, 0x82, 0xD0 };
+uint8_t teamA[] = { 0xA8, 0x42, 0xE3, 0xBC, 0x72, 0xF8 }; // Change MAC address to suit your ESP32
+uint8_t teamB[] = { 0xE4, 0x65, 0xB8, 0x79, 0x78, 0xB0 }; // Change MAC address to suit your ESP32
+uint8_t teamC[] = { 0xA0, 0xA3, 0xB3, 0xAB, 0x84, 0x24 }; // Change MAC address to suit your ESP32
+uint8_t teamD[] = { 0xE4, 0x65, 0xB8, 0x79, 0x82, 0xD0 }; // Change MAC address to suit your ESP32
 
 // define I2C address for 16x2 LCD
 #define I2C_ADDR 0x27
